43bbbc5923e7d1e9752f11c4a9d7e2ee37d1f8de
Takafumi Suzuki <tk4fm1@gmail.com>
2020-08-21 13:51:38 +0900

    init

--- SLIM/SLIM_model.py
+++ SLIM/SLIM_model.py
@@ -6,6 +6,8 @@ from multiprocessing import Pool
 import multiprocessing as multi
 from joblib import Parallel, delayed
 
+#from pyglmnet import GLM
+
 
 class SLIM():
     
@@ -15,9 +17,11 @@ class SLIM():
             self.reg = Lasso(alpha=alpha, positive=True)
         elif lin_model == 'elastic':
             self.reg = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, positive=True)
+            #self.reg = GLM(distr='gaussian', alpha=l1_ratio, reg_lambda=alpha)
             
         self.user_num = user_num
         self.item_num = item_num
+
             
     def fit(self, user_item_train_df):
         # rating_mat
@@ -36,10 +40,11 @@ class SLIM():
             w = np.insert(self.reg.coef_, i, 0)[:,  np.newaxis]
             sim_mat.append(w)
     
-            #if i > 100:
+            #if i > 1:
             #    break
 
         self.sim_mat = np.concatenate(sim_mat, axis=1)
+
 
     def fit_multi(self, user_item_train_df):
         # rating_mat
@@ -51,6 +56,7 @@ class SLIM():
         # linear modelを解く
         sim_mat = Parallel(n_jobs=-1)([delayed(self.solve_lin_model)(n) for n in range(self.item_num)])
         self.sim_mat = np.concatenate(sim_mat, axis=1)
+
         
     def solve_lin_model(self, idx):
         X = self.del_col(idx)
@@ -58,7 +64,6 @@ class SLIM():
         self.reg.fit(X.toarray(), y.toarray())
         w = np.insert(self.reg.coef_, idx, 0)[:,  np.newaxis]
         return w
-
         
 
     def del_col(self, col_idx):
@@ -68,6 +73,7 @@ class SLIM():
         data_new = self.data[self.col != col_idx]
     
         return csr_matrix((data_new, (row_new, col_new)), shape = (self.user_num, self.item_num-1))
+
     
     def load_sim_mat(self, path, user_item_train_df):
         # rating mat
@@ -77,16 +83,20 @@ class SLIM():
         self.rating_mat = csr_matrix((self.data, (self.row, self.col)), shape = (self.user_num, self.item_num))
         
         self.sim_mat = np.loadtxt(path)
+
     
     def save_sim_mat(self, path):
         np.savetxt(path, self.sim_mat)
-        
-    def pred_ranking(self, user_id):
-        pred_mat = np.dot(self.rating_mat.toarray(), self.sim_mat)
 
+        
+    def predict(self):
+        pred_mat = np.dot(self.rating_mat.toarray(), self.sim_mat)
+        self.rec_mat = pred_mat - self.rating_mat
+
+    def pred_ranking(self, user_id):
         # あるユーザの予測ランキングを返す
-        rec_mat = pred_mat - self.rating_mat
-        row_user = rec_mat[user_id, :]
+        #rec_mat = self.pred_mat - self.rating_mat
+        row_user = self.rec_mat[user_id, :]
         #print(row_user)
         rec_item_idx = np.argsort(row_user)[::-1]
 
